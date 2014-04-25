TNG/junit-dataprovider

commit d0c256600d23dde87b4d3bb402ad0ae22a762950

fixed #16

src/main/java/com/tngtech/java/junit/dataprovider/DataProviderRunner.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
-import org.junit.experimental.categories.Categories;
+import org.junit.experimental.categories.Categories.CategoryFilter;
 import org.junit.runner.manipulation.Filter;
 import org.junit.runner.manipulation.NoTestsRemainException;
 import org.junit.runners.BlockJUnit4ClassRunner;
@@ -40,7 +40,13 @@ public class DataProviderRunner extends BlockJUnit4ClassRunner {
 
     @Override
     public void filter(Filter filter) throws NoTestsRemainException {
-        super.filter((filter instanceof Categories.CategoryFilter) ? filter : new DataProviderFilter(filter));
+        Filter useFilter;
+        if (!(filter instanceof CategoryFilter) && filter.getClass().getName().startsWith("org.junit")) {
+            useFilter = new DataProviderFilter(filter);
+        } else {
+            useFilter = filter;
+        }
+        super.filter(useFilter);
     }
 
     @Override

src/test/java/com/tngtech/java/junit/dataprovider/DataProviderRunnerTest.java
@@ -17,6 +17,7 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Categories;
+import org.junit.runner.Description;
 import org.junit.runner.manipulation.Filter;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.TestClass;
@@ -60,10 +61,32 @@ public class DataProviderRunnerTest {
     }
 
     @Test
-    public void testFilterShouldNotThrowExceptionForCategoryFilter() throws Exception {
+    public void testFilterShouldNotThrowExceptionForJUnitCategoryFilter() throws Exception {
 
         // Given:
         Filter filter = new Categories.CategoryFilter(null, CategoryOne.class);
+
+        // When:
+        underTest.filter(filter);
+
+        // Then: expect no exception
+    }
+
+    @Test
+    public void testFilterShouldNotThrowExceptionForNonJUnitFilter() throws Exception {
+
+        // Given:
+        Filter filter = new Filter() {
+            @Override
+            public boolean shouldRun(Description description) {
+                return true;
+            }
+
+            @Override
+            public String describe() {
+                return "test filter in package 'com.tngtech.java.dataprovider' which runs all tests";
+            }
+        };
 
         // When:
         underTest.filter(filter);

commit 076288d384ffde25cb123a28d582ee1d03bd81ef

Fix issue #18: Unable to execute a single test in latest IntelliJ
This bug was introduced in d0c256600. The fix is to blacklist "bad"
filter packages instead of whitelisting the "good" ones.

src/main/java/com/tngtech/java/junit/dataprovider/DataProviderRunner.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -31,6 +32,12 @@ public class DataProviderRunner extends BlockJUnit4ClassRunner {
     List<FrameworkMethod> computedTestMethods;
 
     /**
+     * A list of filter packages which must not be wrapped by DataProviderRunner
+     * (this is a workaround for some plugins, e.g. the maven-surefire-plugin).
+     */
+    private static final List<String> BLACKLISTED_FILTER_PACKAGES = Arrays.asList("org.apache.maven.surefire");
+
+    /**
      * Creates a DataProviderRunner to run supplied {@code clazz}.
      *
      * @param clazz the test {@link Class} to run
@@ -43,7 +50,7 @@ public class DataProviderRunner extends BlockJUnit4ClassRunner {
     @Override
     public void filter(Filter filter) throws NoTestsRemainException {
         Filter useFilter;
-        if (!(filter instanceof CategoryFilter) && filter.getClass().getName().startsWith("org.junit")) {
+        if (!(filter instanceof CategoryFilter) && !isFilterBlackListed(filter)) {
             useFilter = new DataProviderFilter(filter);
         } else {
             useFilter = filter;
@@ -280,5 +287,15 @@ public class DataProviderRunner extends BlockJUnit4ClassRunner {
      */
     TestClass getTestClassInt() {
         return getTestClass();
+    }
+
+    private boolean isFilterBlackListed(Filter filter) {
+        String className = filter.getClass().getName();
+        for (String blacklistedPackage: BLACKLISTED_FILTER_PACKAGES) {
+            if (className.startsWith(blacklistedPackage)) {
+                return true;
+            }
+        }
+        return false;
     }
 }