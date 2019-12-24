7768159594f32db96fd3855ee5717b3dbaee285a
Andreas Schmid <service@aaschmid.de>
2019-12-24 21:16:37 +0100

    migrate remaining publishing configuration to Kotlin DSL (#123)

--- build.gradle.kts
+++ build.gradle.kts
@@ -390,6 +390,119 @@ coveralls {
     jacocoReportPath = "${buildDir}/reports/jacoco/jacocoRootReport/jacocoRootReport.xml"
 }
 
+// -- sign and publish artifacts -------------------------------------------------------------------------------------
+val isReleaseVersion by extra(!project.version.toString().endsWith("-SNAPSHOT"))
+
+// username and password from gradle.properties otherwise empty
+val sonatypeUsername by extra(findProperty("sonatypeUsername")?.toString() ?: "")
+val sonatypePassword by extra(findProperty("sonatypePassword")?.toString() ?: "")
+
+subprojects {
+    apply<MavenPublishPlugin>()
+    apply<SigningPlugin>()
+
+    tasks.withType<GenerateModuleMetadata> {
+        enabled = isReleaseVersion // signing of these artifacts causes failure for snapshot versions
+    }
+
+    configure<PublishingExtension> {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                val archivesBaseName = project.the<BasePluginConvention>().archivesBaseName
+                artifactId = archivesBaseName
+                from(components["java"])
+                pom {
+                    packaging = "jar"
+
+                    name.set(archivesBaseName)
+                    description.set(project.description)
+                    url.set("https://github.com/TNG/junit-dataprovider")
+
+                    developers {
+                        developer {
+                            id.set("aaschmid")
+                            name.set("Andreas Schmid")
+                            email.set("service@aaschmid.de")
+                        }
+                    }
+
+                    licenses {
+                        license {
+                            name.set("The Apache Software License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                            distribution.set("repo")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git@github.com:TNG/junit-dataprovider.git")
+                        developerConnection.set("scm:git@github.com:TNG/junit-dataprovider.git")
+                        url.set("scm:git@github.com:TNG/junit-dataprovider.git")
+                    }
+
+                    withXml {
+                        fun org.w3c.dom.NodeList.asElementList() = (0 until length).map { this::item }.filterIsInstance<org.w3c.dom.Element>()
+
+                        asElement()
+                                .getElementsByTagName("dependencies")
+                                .asElementList()
+                                .flatMap { deps -> deps.childNodes.asElementList() }
+                                .forEach { dep ->
+                                    val groupId = dep.getElementsByTagName("groupId").item(0)
+                                    val artifactId = dep.getElementsByTagName("artifactId").item(0)
+
+                                    // JUnit4
+                                    if (groupId.textContent == "junit" && artifactId.textContent == "junit") {
+                                        dep.getElementsByTagName("version").item(0).textContent = "[4.10,4.12]"
+                                        dep.getElementsByTagName("scope").item(0).textContent = "provided"
+                                    }
+
+                                    // JUnit5
+                                    if ((groupId.textContent == "org.junit.jupiter" && artifactId.textContent == "junit-jupiter-engine") ||
+                                            (groupId.textContent == "org.junit.jupiter" && artifactId.textContent == "junit-jupiter-params")) {
+                                        dep.getElementsByTagName("version").item(0).textContent = "[5.5.0-M6,6.0.0)"
+                                        dep.getElementsByTagName("scope").item(0).textContent = "provided"
+                                    }
+                                }
+                    }
+                }
+                // finally call `asNode` to get rid of excessive newlines caused by use of asElement
+                // see also https://github.com/gradle/gradle/issues/7529
+                afterEvaluate {
+                    pom {
+                        withXml {
+                            asNode()
+                        }
+                    }
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                val snapshotRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+                url = if (isReleaseVersion) releasesRepoUrl else snapshotRepoUrl
+
+                credentials {
+                    username = sonatypeUsername
+                    password = sonatypePassword
+                }
+
+                metadataSources {
+                    gradleMetadata()
+                }
+            }
+        }
+    }
+
+    // requires gradle.properties, see http://www.gradle.org/docs/current/userguide/signing_plugin.html
+    configure<SigningExtension> {
+        setRequired({ isReleaseVersion && gradle.taskGraph.hasTask("publish") })
+        sign(the<PublishingExtension>().publications["mavenJava"])
+    }
+}
+
 // -- Custom tasks ------------------------------------------------------------
 /**
  * Task to touch all junit xml report files for all given {@link Test} {@code tasks}.
@@ -418,5 +531,3 @@ open class TouchTestResults : DefaultTask() {
         }
     }
 }
-
-apply(from = "legacy.build.gradle")
