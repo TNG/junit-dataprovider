0efbd393ac6694aa1c8da25aaa161825256ff026
Andreas Schmid <service@aaschmid.de>
2019-12-25 17:50:32 +0100

    use Bnd Gradle Plugins via plugins {} API
    Workaround described in https://github.com/bndtools/bnd/issues/3174.
    There are also more information provided.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
+pluginManagement {
+    repositories {
+        mavenCentral() // required as first repo for Bnd Gradle Plugins, see also https://github.com/bndtools/bnd/issues/3174
+        gradlePluginPortal()
+    }
+}
 plugins {
     id("com.gradle.enterprise").version("3.1.1")
 }
